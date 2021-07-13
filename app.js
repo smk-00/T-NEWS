404a51300bfb90f413de018c3ff4b1d1fe71c303
MANOJ KUMAR <smk11500@gmail.com>
2021-07-13 23:31:23 +0530

    commit

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ if ((alltopics === null && headlines === null) || status) {
   isToStore = true;
 }
 
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8000;
 setTimeout(() => {
   app.listen(PORT, () => {
